47b93ff812f42760be42635e506617d4932d0d86
Jonathan Rodrigues <jonrodrigues24@gmail.com>
2021-06-15 09:17:01 -0600

    modified build to include docs/api

--- src/main/java/edu/cnm/deepdive/Shuffler.java
+++ src/main/java/edu/cnm/deepdive/Shuffler.java
@@ -141,4 +141,23 @@ public class Shuffler {
 
 
 
+
+  public boolean hasTeen(int a, int b, int c) {
+
+
+
+    boolean result = false;
+
+
+    result = (a >= 13 && a <= 19) || (b >= 13 && b <= 19) || (c >= 13 && c <= 19);
+
+
+    return result;
+
+
+  }
+
+
+
+
 }
